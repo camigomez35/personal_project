7dd9c056be340b83dc2dc82712f7cfed981012b2
Maria Camila Gomez <camigomez35@gmail.com>
2019-06-27 01:21:30 -0500

    Init config

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <HelloWorld name="Camila"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+#app {
+  font-size: 18px;
+  font-family: 'Roboto', sans-serif;
+  color: blue;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Hello {{ name }}!</h1>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      name: "Camila!"
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
